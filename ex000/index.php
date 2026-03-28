bfba19820e68c3650b03c5fcb644a9b5d9e93df1
Samuel <samuel.fsbs@gmail.com>
2026-03-28 09:35:09 -0300

    Comando básico em PHP.

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me Livrando da Maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá mundo \u{1F30E}";
+            
+            print "Olá Julia";
+        ?>
+    </h1>
+
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
