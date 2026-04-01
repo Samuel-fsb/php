eafcaaee02ffe358fb4be6d5224008aceea5dd4f
Samuel <samuel.fsbs@gmail.com>
2026-04-01 15:21:09 -0300

    Criando um exercício de interpolação de variáveis.

---
+++ ex003/interpolacao.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Intepolação de Aspas Simples e Duplas</h1>
+
+    <?php 
+    const Estado = "RJ";
+    echo "Eu moro no estado " . Estado;
+
+    echo "Estamos no ano de " . date('Y'); //Com essas aspas duplas o PHP vai interpretar as variáveis.
+    
+    ?>
+
+
+
+
+</body>
+</html>
