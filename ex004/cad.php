cc4125c0a9df2aa95dfde55b3502529979f38f9f
Samuel <samuel.fsbs@gmail.com>
2026-04-06 14:33:33 -0300

    Usando o operador de coalescencia nula.

--- ex004/cad.php
+++ ex004/cad.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -14,11 +15,11 @@
 
     <main>
         <?php 
-           $nome = $_GET["nome"];
-           $sobrenome = $_GET["sobrenome"];
-            echo "<p>É um prazer em lhe conhecer, $nome $sobrenome! Esse é o meu site!</p>";
-        ?>
+           $nome = $_GET["nome"] ?? "Sem Nome";
+           $sobrenome = $_GET["email"] ?? "Sem Email";
+            echo "<p>É um prazer em lhe conhecer, <strong>$nome $sobrenome</strong>! Esse é o meu site!</p>";
+            ?>
+            <a href="index.html">Voltar página</a>
     </main>
-    
 </body>
 </html>
