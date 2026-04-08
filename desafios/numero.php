e2e4b79f0bd37649f6b567e86db970fa0034eb6c
Samuel <samuel.fsbs@gmail.com>
2026-04-08 14:24:16 -0300

    Finalizando exercício de sucessor e antecessor.

--- desafios/numero.php
+++ desafios/numero.php
@@ -5,17 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
     <title>Resultado</title>
+    <style>
+
+        section{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+
+        button{
+          padding: 20px 60px;
+          cursor: pointer;
+        }
+
+        a{
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     <section>
         <h1>Resultado Final</h1>
-         <?php
-            $antecessor = $numero - 1;
-            $sucessor = $numero + 1;
-             echo "O número escolhido foi $numero";
-             echo "O seu antecessor é $antecessor";
-             echo "O seu sucessor é $sucessor";
-         ?>
+             <?php
+                $numero = $_GET['numero'] ?? 0;
+                $antecessor = $numero - 1;
+                $sucessor = $numero + 1;
+                 echo "<p>O número escolhido foi <strong>$numero</strong></p>";
+                 echo "<p>O seu antecessor é <strong>$antecessor</strong></p>";
+                 echo "<p>O seu sucessor é <strong>$sucessor</strong></p>";
+             ?>
              <button><a href="exd001.html">Voltar</a></button>
     </section>
 </body>
