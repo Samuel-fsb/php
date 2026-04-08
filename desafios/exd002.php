60315a371ec4f2145d293981609899553b224c49
Samuel <samuel.fsbs@gmail.com>
2026-04-08 14:30:20 -0300

    Criando a base do gerador de números.

---
+++ desafios/exd002.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerando Números Aleatórios</title>
+    <link rel="stylesheet" href="../style.css">
+    <style>
+
+    </style>
+</head>
+<body>
+    <section>
+        <h1>Trabalhando com números aleatórios</h1>
+
+        <p>Gerando um número aleatório entre 0 e 100...</p>
+        <?php 
+        
+        echo "O valor gerado foi ";
+
+        ?>
+    </section>
+</body>
+</html>
