631588e7ca1dd2d0746ebccd0e8586a08ea8831a
Samuel <samuel.fsbs@gmail.com>
2026-04-10 12:58:00 -0300

    Ajustando as datas da API, fazendo conversão e mostrando na tela do usuário.

--- desafios/conversor.php
+++ desafios/conversor.php
@@ -38,10 +38,15 @@
     <h1>Resultado da Conversão</h1>
     <section>
         <?php
+        $Inicio = date("m-d-Y", strtotime("-7days"));
+        $Fim = date("m-d-Y");
         $ValorTotal = $_GET["reais"] ?? 0;
-        $ValorJSON = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'04-02-2026\'&@dataFinalCotacao=\'04-09-2026\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+        $ValorJSON = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$Inicio .'\'&@dataFinalCotacao=\''.$Fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&\$select=cotacaoCompra,dataHoraCotacao';
         $ValorDaAPI = json_decode(file_get_contents($ValorJSON), true);
-        $ValorConvertido = $ValorTotal / $ValorDaAPI;
+        
+        $Cotacao = $ValorDaAPI["value"][0]["cotacaoCompra"];
+        
+        $ValorConvertido = $Cotacao / $ValorTotal;
         echo "A conversão do seu dinheiro em dólares é $ValorConvertido";
         ?>
         <button><a href="exd004.html">Voltar</a></button>
